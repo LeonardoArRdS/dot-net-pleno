f14e1e1545359448a63be5bce49c9a4ebbdfb789
Leonardo Rodrigues <127156962+LeonardoArRdS@users.noreply.github.com>
2024-07-27 00:27:30 -0300

    Camada Domain - Extensions

---
+++ StallosDotnetPleno.Domain/Extensions/EnumExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace StallosDotnetPleno.Domain.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            FieldInfo field = value.GetType().GetField(value.ToString());
+
+            DescriptionAttribute attribute = field.GetCustomAttribute<DescriptionAttribute>();
+
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+    }
+}
